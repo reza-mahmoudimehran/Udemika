b2096d9e05933632a2b5b8f5130dce08319d600a
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2022-07-28 18:59:31 +0430

    =Refactor fragment_home Layout

--- app/src/main/java/ir/reza_mahmoudi/udemika/fragment/home/HomeFragment.kt
+++ app/src/main/java/ir/reza_mahmoudi/udemika/fragment/home/HomeFragment.kt
@@ -1,10 +1,11 @@
-package ir.reza_mahmoudi.udemika
+package ir.reza_mahmoudi.udemika.fragment.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ir.reza_mahmoudi.udemika.R
 
 
 class HomeFragment : Fragment() {
--- app/src/main/java/ir/reza_mahmoudi/udemika/fragment/splash/SplashScreenFragment.kt
+++ app/src/main/java/ir/reza_mahmoudi/udemika/fragment/splash/SplashScreenFragment.kt
@@ -1,16 +1,15 @@
-package ir.reza_mahmoudi.udemika
+package ir.reza_mahmoudi.udemika.splash
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import ir.reza_mahmoudi.udemika.R
 import ir.reza_mahmoudi.udemika.databinding.FragmentSplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
